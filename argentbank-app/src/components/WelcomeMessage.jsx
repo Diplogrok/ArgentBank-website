eae0469edba084917214d028d349493c2c4809fe
Alexia Stephan <alexia0stephan@gmail.com>
2024-05-07 12:00:08 +0200

    correction

--- argentbank-app/src/components/WelcomeMessage.jsx
+++ argentbank-app/src/components/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ function WelcomeMessage({ firstName, lastName }) {
   return (
     <h1 className="font-bold text-3xl pb-6">
       Welcome Back <br />
-      {`${firstName} ${lastName}`}
+      {`${firstName} ${lastName}`}!
     </h1>
   );
 }
